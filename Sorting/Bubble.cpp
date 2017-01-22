431bffa75a58866b01ba460eb036b02ab8d42a0c
youngjjj <2275169040@qq.com>
2017-01-22 23:37:47 +0800

    冒泡排序

--- Sorting/Bubble.cpp
+++ Sorting/Bubble.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 using namespace std;
+
 void Swap(int *a,int *b){
-	if(a>b){
-		int temp=*a;
-		*a=*b;
-		*b=temp;
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
+void Bubble(int *a,int size){
+	for(int i=size;i>0;i--){
+		for(int j=0;j<i;j++){
+			if(a[j]>a[j+1]){
+				Swap(a+j,a+j+1);
+			}
+		}
 	}
 }
 
-
 int main(){
-	int x,y;
-	cin>>x>>y;
-	cout<<x<<y;
-	Swap(&x,&y);
-	cout<<x<<y;
-	system("pause");
-	return 0;
+	int n;
+	cin>>n;
+	int *a = new int[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<"  ";
+	}
+	cout<<endl;
+	Bubble(a,n-1);
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<"  ";
+	}
+	
 }
 
